7ee0671c644b23a9fe71fcb4365b37f0e99095bb
Dave <davemers@iu.edu>
2023-08-21 19:47:59 -0400

    updating fsk generation

--- signal_generation/fsk_signal_gen.py
+++ signal_generation/fsk_signal_gen.py
@@ -4,20 +4,23 @@ import yaml
 
 import numpy as np
 
-def generate_fsk(data, num_bits, amplitude, sample_rate, bit_length, freq_separation):
+def generate_fsk(data, num_bits, amplitude, sample_rate, bit_length, center_freq, freq_separation):
 
     samples_per_bit = math.floor(sample_rate * bit_length)
-    freq_offset = (freq_separation / 2.0) / sample_rate
+    # freq_offset = (freq_separation / 2.0) / sample_rate
+
+    f1 = (center_freq - freq_separation/2.0)/sample_rate;
+    f2 = (center_freq + freq_separation/2.0)/sample_rate;
 
     iq = np.empty(0)
 
     for idx in range(num_bits):
 
         if data[idx] == 1:
-            tmp_iq = amplitude * (np.exp(1j * 2 * np.pi * freq_offset * np.arange(0, samples_per_bit)))
+            tmp_iq = amplitude * (np.exp(1j * 2 * np.pi * f1 * np.arange(0, samples_per_bit)))
 
         else:
-            tmp_iq = amplitude * (np.exp(1j * 2 * np.pi * -freq_offset * np.arange(0, samples_per_bit)))
+            tmp_iq = amplitude * (np.exp(1j * 2 * np.pi * f2 * np.arange(0, samples_per_bit)))
 
         iq = np.concatenate((iq, tmp_iq))
 
@@ -28,12 +31,13 @@ def read_input_params(filename):
     with open(filename, 'r') as file:
         fsk_params = yaml.safe_load(file)
 
-    return fsk_params['data_rate'], fsk_params['num_bits'], fsk_params['frame_length'], fsk_params['amplitude'], fsk_params['sample_rate']
+    return fsk_params['data_rate'], fsk_params['num_bits'], fsk_params['frame_length'], fsk_params['amplitude'], \
+           fsk_params['center_frequency'], fsk_params['sample_rate']
 
 
 filename = "D:/data/FSK/fsk_test.yml"
 
-data_rate, num_bits, frame_length, amplitude, sample_rate = read_input_params(filename)
+data_rate, num_bits, frame_length, amplitude, center_freq, sample_rate = read_input_params(filename)
 
 #
 bit_length = 1.0/data_rate
@@ -42,7 +46,7 @@ bit_length = 1.0/data_rate
 data_length = num_bits * bit_length
 
 #
-buffer_length = (frame_length - data_length)/2
+buffer_length = (frame_length - data_length)
 
 #
 freq_separation = 2 * data_rate
@@ -53,10 +57,9 @@ data = np.random.randint(2, size=num_bits)
 #
 sig_off = 0.001 * amplitude * (np.random.randn(math.floor(sample_rate * buffer_length)) + 1.j * np.random.randn(math.floor(sample_rate * buffer_length)))
 
-iq = generate_fsk(data, num_bits, amplitude, sample_rate, bit_length, freq_separation)
+iq = generate_fsk(data, num_bits, amplitude, sample_rate, bit_length, center_freq, freq_separation)
 
-full_signal = np.concatenate((sig_off, iq, sig_off))
-
+full_signal = np.concatenate((iq, sig_off))
 
 data_flat = np.empty(2 * len(full_signal))
 data_flat[0::2] = np.real(full_signal)
